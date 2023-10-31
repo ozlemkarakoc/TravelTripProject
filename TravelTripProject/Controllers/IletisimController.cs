a85e43fb33df5fdf553ed70c8176c6e8cba8c7af
ozlmk <ozlmk@OZLEMSMONSTER>
2023-11-01 00:59:01 +0300

    Proje dosyası ekle.

---
+++ TravelTripProject/Controllers/IletisimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace TravelTripProject.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+  
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
